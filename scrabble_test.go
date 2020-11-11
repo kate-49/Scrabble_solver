bea671ba73b4c8eb52dd87118718479c95bd4a9b
kate smith <kateesmith4994@gmail.com>
2020-11-11 11:59:45 +0000

    managed to reintroduce multiple inputs in test

--- scrabble_test.go
+++ scrabble_test.go
@@ -2,49 +2,21 @@ package scrabble
 
 import "testing"
 
-func TestScrabble(t *testing.T) {
+func TestLettersWithValue1IncludedNonValueLetters(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  int
+	}{
+		{input: []string{"A", "E", "I", "O", "U"}, want: 5},
+		{input: []string{"A", "B", "C", "E", "I", "K", "O", "X"}, want: 4},
+		{input: []string{"L", "N", "R", "S", "T"}, want: 5},
+		{input: []string{"N", "E", "U", "X"}, want: 3},
+	}
 
-	t.Run("Test that 5 letters of 1 value letters return 5", func(t *testing.T) {
-		got := Run([]string{"A", "E", "I", "O", "U"})
-		want := 5
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
+	for _, tc := range tests {
+		got := Run(tc.input)
+		if tc.want != got {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
-
-	})
-	t.Run("Test mix of 1 value letters and other letters", func(t *testing.T) {
-		got := Run([]string{"A", "B", "C", "E", "I", "K", "O", "X"})
-		want := 4
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
-
-	})
+	}
 }
-
-// func TestLettersWithValue1(t *testing.T) {
-// 	tests := []struct {
-// 		input string
-// 		want  int
-// 	}{
-// 		{input: "A", want: 1},
-// 		{input: "E", want: 1},
-// 		{input: "I", want: 1},
-// 		{input: "0", want: 1},
-// 		{input: "U", want: 1},
-// 		{input: "L", want: 1},
-// 		{input: "N", want: 1},
-// 		{input: "R", want: 1},
-// 		{input: "S", want: 1},
-// 		{input: "T", want: 1},
-// 	}
-
-// 	for _, tc := range tests {
-// 		got := Run(tc.input)
-// 		if tc.want != got {
-// 			t.Errorf("expected: %v, got: %v", tc.want, got)
-// 		}
-// 	}
-// }

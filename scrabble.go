360951fde214999af8c9ad8aeb1d582c029281cd
kate smith <kateesmith4994@gmail.com>
2020-11-11 12:04:13 +0000

    passing test for 2 point letters

--- scrabble.go
+++ scrabble.go
@@ -4,11 +4,17 @@ func Run(inputLetters []string) int {
 	var sum int
 
 	OneValueLetters := [10]string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}
+	TwoValueLetters := [2]string{"D", "G"}
 
 	for _, letter := range inputLetters {
 		for _, oneValueLetter := range OneValueLetters {
 			if letter == oneValueLetter {
 				sum++
+			}
+		}
+		for _, TwoValueLetter := range TwoValueLetters {
+			if letter == TwoValueLetter {
+				sum += 2
 			}
 		}
 	}

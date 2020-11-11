095d0ab2e7cc05253a57f5d5fcae82f3fd861b62
kate smith <kateesmith4994@gmail.com>
2020-11-11 11:26:08 +0000

    added rest of letters

--- scrabble_test.go
+++ scrabble_test.go
@@ -10,6 +10,13 @@ func TestLettersWithValue1(t *testing.T) {
 		{input: "A", want: 1},
 		{input: "E", want: 1},
 		{input: "I", want: 1},
+		{input: "0", want: 1},
+		{input: "U", want: 1},
+		{input: "L", want: 1},
+		{input: "N", want: 1},
+		{input: "R", want: 1},
+		{input: "S", want: 1},
+		{input: "T", want: 1},
 	}
 
 	for _, tc := range tests {

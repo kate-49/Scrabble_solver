fe44c85e038511c82cfc9249c05af62d2a7d172e
kate smith <kateesmith4994@gmail.com>
2020-11-11 11:55:10 +0000

    test to ensure other value letters aren't being counter with 1 value letters

--- scrabble_test.go
+++ scrabble_test.go
@@ -4,9 +4,18 @@ import "testing"
 
 func TestScrabble(t *testing.T) {
 
-	t.Run("Test that letter A returns 1", func(t *testing.T) {
+	t.Run("Test that 5 letters of 1 value letters return 5", func(t *testing.T) {
 		got := Run([]string{"A", "E", "I", "O", "U"})
 		want := 5
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+
+	})
+	t.Run("Test mix of 1 value letters and other letters", func(t *testing.T) {
+		got := Run([]string{"A", "B", "C", "E", "I", "K", "O", "X"})
+		want := 4
 
 		if got != want {
 			t.Errorf("got %d want %d", got, want)

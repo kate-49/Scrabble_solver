4d3180bc1c603d136f6b2c8db51169784058d016
kate smith <kateesmith4994@gmail.com>
2020-11-12 16:11:16 +0000

    added catch for nil

--- scrabble.go
+++ scrabble.go
@@ -5,16 +5,19 @@ import "strings"
 func Run(inputLetters string) int {
 	var sum int
 
-	OneValueLetters := [10]string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}
-	TwoValueLetters := [2]string{"D", "G"}
-	ThreeValueLetters := [4]string{"B", "C", "M", "P"}
-	FourValueLetters := [5]string{"F", "H", "V", "W", "Y"}
-	FiveValueLetters := [1]string{"K"}
-	EightValueLetters := [2]string{"J", "X"}
-	TenValueLetters := [2]string{"Q", "Z"}
+	OneValueLetters := []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}
+	TwoValueLetters := []string{"D", "G"}
+	ThreeValueLetters := []string{"B", "C", "M", "P"}
+	FourValueLetters := []string{"F", "H", "V", "W", "Y"}
+	FiveValueLetters := []string{"K"}
+	EightValueLetters := []string{"J", "X"}
+	TenValueLetters := []string{"Q", "Z"}
 
-	arrayLength := len(inputLetters)
-	inputLettersArray := strings.SplitAfterN(inputLetters, "", arrayLength)
+	if inputLetters == "nil" {
+		return sum
+	}
+
+	inputLettersArray := strings.SplitAfterN(inputLetters, "", len(inputLetters))
 
 	for _, letter := range inputLettersArray {
 		for _, oneValueLetter := range OneValueLetters {

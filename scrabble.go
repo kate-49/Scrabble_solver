91ea5c96f69df2a2b4610d5d1b0f3e294583ab0a
kate smith <kateesmith4994@gmail.com>
2020-11-12 16:38:05 +0000

    refactored to use structs

--- scrabble.go
+++ scrabble.go
@@ -5,13 +5,35 @@ import "strings"
 func Run(inputLetters string) int {
 	var sum int
 
-	OneValueLetters := []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}
-	TwoValueLetters := []string{"D", "G"}
-	ThreeValueLetters := []string{"B", "C", "M", "P"}
-	FourValueLetters := []string{"F", "H", "V", "W", "Y"}
-	FiveValueLetters := []string{"K"}
-	EightValueLetters := []string{"J", "X"}
-	TenValueLetters := []string{"Q", "Z"}
+	type Letters struct {
+		Let string
+	}
+
+	type ScrabbleLetters struct {
+		LetterValue []string
+	}
+
+	one := ScrabbleLetters{
+		LetterValue: []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"},
+	}
+	two := ScrabbleLetters{
+		LetterValue: []string{"D", "G"},
+	}
+	three := ScrabbleLetters{
+		LetterValue: []string{"B", "C", "M", "P"},
+	}
+	four := ScrabbleLetters{
+		LetterValue: []string{"F", "H", "V", "W", "Y"},
+	}
+	five := ScrabbleLetters{
+		LetterValue: []string{"K"},
+	}
+	eight := ScrabbleLetters{
+		LetterValue: []string{"J", "X"},
+	}
+	ten := ScrabbleLetters{
+		LetterValue: []string{"Q", "Z"},
+	}
 
 	if inputLetters == "nil" {
 		return sum
@@ -20,37 +42,37 @@ func Run(inputLetters string) int {
 	inputLettersArray := strings.SplitAfterN(inputLetters, "", len(inputLetters))
 
 	for _, letter := range inputLettersArray {
-		for _, oneValueLetter := range OneValueLetters {
+		for _, oneValueLetter := range one.LetterValue {
 			if strings.ToUpper(letter) == oneValueLetter {
 				sum++
 			}
 		}
-		for _, TwoValueLetter := range TwoValueLetters {
+		for _, TwoValueLetter := range two.LetterValue {
 			if strings.ToUpper(letter) == TwoValueLetter {
 				sum += 2
 			}
 		}
-		for _, ThreeValueLetter := range ThreeValueLetters {
+		for _, ThreeValueLetter := range three.LetterValue {
 			if strings.ToUpper(letter) == ThreeValueLetter {
 				sum += 3
 			}
 		}
-		for _, FourValueLetter := range FourValueLetters {
+		for _, FourValueLetter := range four.LetterValue {
 			if strings.ToUpper(letter) == FourValueLetter {
 				sum += 4
 			}
 		}
-		for _, FiveValueLetter := range FiveValueLetters {
+		for _, FiveValueLetter := range five.LetterValue {
 			if strings.ToUpper(letter) == FiveValueLetter {
 				sum += 5
 			}
 		}
-		for _, EightValueLetter := range EightValueLetters {
+		for _, EightValueLetter := range eight.LetterValue {
 			if strings.ToUpper(letter) == EightValueLetter {
 				sum += 8
 			}
 		}
-		for _, TenValueLetter := range TenValueLetters {
+		for _, TenValueLetter := range ten.LetterValue {
 			if strings.ToUpper(letter) == TenValueLetter {
 				sum += 10
 			}
